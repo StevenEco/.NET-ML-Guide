2515c56fa583619837de6baf804e5fcf7ac428ac
WarrenRyan <cxtionch@live.com>
2020-01-14 19:09:54 +0800

    Add Logistic Template

---
+++ ML In Action/Logistic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
