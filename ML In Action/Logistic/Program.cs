37643194c42b88c1a7de1558ff972f2ddcb973d0
WarrenRyan <cxtionch@live.com>
2020-01-16 19:16:44 +0800

    update

--- ML In Action/Logistic/Program.cs
+++ ML In Action/Logistic/Program.cs
@@ -30,7 +30,7 @@ namespace Logistic
 
         private double Sigmoid(double x)
         {
-            return (double)1 / Math.Exp(x);
+            return (double)1 / (1 + Math.Exp(-1 * x));
         }
 
         private model1[] InitDataSet()
@@ -58,7 +58,10 @@ namespace Logistic
             return ap;
         }
 
-        private void Graph(string[] args, double[] x1, double[] y1, double[] x2, double[] y2)
+        private void Graph(string[] args,
+        double[] x1, double[] y1,
+        double[] x2, double[] y2,
+        double[] x3, double[] y3)
         {
             string chartFileName = string.Empty;
             var pl = new PLStream();
@@ -82,13 +85,14 @@ namespace Logistic
             const int yMin = -5;
             const int yMax = 20;
             pl.env(xMin, xMax, yMin, yMax, AxesScale.Independent, AxisBox.CustomXYBoxTicksLabels);
-
             // Set scaling for mail title text 125% size of default
             pl.lab("X1", "X2", "Title");
             pl.col0(3);
             pl.sym(x1, y1, (char)228);
             pl.col0(9);
             pl.sym(x2, y2, (char)228);
+            pl.col0(2);
+            pl.line(x3, y3);
             pl.eop();
             pl.gver(out var verText);
             var p = new Process();
@@ -100,17 +104,51 @@ namespace Logistic
             p.Start();
         }
 
+        private double[] GradAscent(model1[] data, double alpha, double[] weight, int maxCircle)
+        {
+            while (maxCircle >= 0)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    var d = data[i].X1 * weight[0] + data[i].X2 * weight[1] + weight[2];
+                    var fx = Sigmoid(d);
+                    var error = data[i].Lable - fx;
+                    weight = weight.Select(p => p + alpha * d * error).ToArray();
+                }
+                maxCircle--;
+            }
+            return weight;
+        }
+
+        public (double[], double[]) InitLine(double[] weight)
+        {
+            (double[], double[]) temp;
+            temp.Item1 = new double[201];
+            temp.Item2 = new double[201];
+            int j = 0;
+            for (double i = -5; i < 5; i = i + 0.05)
+            {
+                temp.Item1[j] = i;
+                temp.Item2[j] = temp.Item1[j] * (-1 * weight[0] / weight[1]) - (weight[2] / weight[1]);
+                j++;
+            }
+            return temp;
+        }
 
         static void Main(string[] args)
         {
             Program p = new Program();
             var data = p.InitDataSet();
-            var X1 = data.Where(p=>p.Lable == 0).Select(p => p.X1).ToArray();
-            var Y1 = data.Where(p=>p.Lable == 0).Select(p => p.X2).ToArray();
-            var X2 = data.Where(p=>p.Lable == 1).Select(p => p.X1).ToArray();
-            var Y2 = data.Where(p=>p.Lable == 1).Select(p => p.X2).ToArray();
-            p.Graph(args,X1,Y1,X2,Y2);
-            Console.WriteLine(p.Sigmoid(1));
+            var X1 = data.Where(p => p.Lable == 0).Select(p => p.X1).ToArray();
+            var Y1 = data.Where(p => p.Lable == 0).Select(p => p.X2).ToArray();
+            var X2 = data.Where(p => p.Lable == 1).Select(p => p.X1).ToArray();
+            var Y2 = data.Where(p => p.Lable == 1).Select(p => p.X2).ToArray();
+            double alpha = 0.01;
+            double[] weight = new double[3] { 1, 1, 1 };
+            weight = p.GradAscent(data, alpha, weight, 500);
+            var line = p.InitLine(weight);
+            p.Graph(args, X1, Y1, X2, Y2, line.Item1, line.Item2);
+            Console.WriteLine();
         }
     }
 }
